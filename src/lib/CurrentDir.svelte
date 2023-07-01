005bdb3bbb8f52ea47832fe7db1c161659adb20c
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-07-01 23:05:12 +0300

    Use SvelteKit insted of Svelte

--- src/lib/CurrentDir.svelte
+++ src/lib/CurrentDir.svelte
@@ -1,58 +1,58 @@
 <script lang="ts">
-    import { invoke} from "@tauri-apps/api/tauri"
-    import SubDir from "./SubDir.svelte";
-  
-    let folder_name = "";
-    let contents = [];
+	import { invoke } from '@tauri-apps/api/tauri';
+	import SubDir from './SubDir.svelte';
 
-    type Filedata = {
-        name: string,
-        path: string,
-        type: string
-    }
+	let folder_name = '';
+	let contents: Array<Filedata> = [];
 
-    type FolderData = {
-        name: string,
-        files: Array<Filedata>
-    }
-    const getCurrentFolder = () => { 
-        invoke("get_current_folder", {}).then((data: FolderData) => {
-            console.log("Getting current folder", data)
-            folder_name = data.name
-            contents = data.files
-        })
-    }
+	type Filedata = {
+		name: string;
+		path: string;
+		filetype: string;
+	};
 
-    const changeDirectory = (path: string) => {
-        contents = []
-        invoke("move_to_folder", {folderPath: path}).then(() => {
-            getCurrentFolder()
-        })
-    }
-    
-    const changeDirectoryToParent = () => {
-        contents = []
-        invoke("move_to_parent_folder").then(() => {
-            getCurrentFolder()
-        })
-    }
+	type FolderData = {
+		name: string;
+		files: Array<Filedata>;
+	};
+	const getCurrentFolder = () => {
+		invoke('get_current_folder', {}).then((data: FolderData) => {
+			console.log('Getting current folder', data);
+			folder_name = data.name;
+			contents = data.files;
+		});
+	};
 
-    getCurrentFolder()
+	const changeDirectory = (path: string) => {
+		contents = [];
+		invoke('move_to_folder', { folderPath: path }).then(() => {
+			getCurrentFolder();
+		});
+	};
+
+	const changeDirectoryToParent = () => {
+		contents = [];
+		invoke('move_to_parent_folder').then(() => {
+			getCurrentFolder();
+		});
+	};
+
+	getCurrentFolder();
 </script>
 
 <div>
-    <h1>{folder_name}</h1>
-    <button on:click={changeDirectoryToParent}>Go Back</button>
-    <ul>
-        {#each contents as content}
-            <li><SubDir folderdata={content} onClick={changeDirectory}/></li>
-        {/each}
-    </ul>
+	<h1>{folder_name}</h1>
+	<button on:click={changeDirectoryToParent}>Go Back</button>
+	<ul>
+		{#each contents as content}
+			<li><SubDir folderdata={content} onClick={changeDirectory} /></li>
+		{/each}
+	</ul>
 </div>
 
 <style>
-    li {
-        margin-bottom: 0.5rem;
-        border-bottom: 2px solid gray;
-    }
+	li {
+		margin-bottom: 0.5rem;
+		border-bottom: 2px solid gray;
+	}
 </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Dir from '../lib/CurrentDir.svelte';
+</script>
+
+<main class="container">
+	<Dir />
+</main>
