b64625af76c559cd94c88f769de09d4ac5e56017
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-07-04 14:42:33 +0300

    Add searchstore

---
+++ src/lib/stores/SearchStore.ts
@@ -0,0 +1,25 @@
+import type { Filedata } from '$lib/dirFunctions';
+import { writable, type Writable } from 'svelte/store';
+
+export type SearchStore = Writable<{
+    data: Filedata[];
+}>;
+
+const searchStore: SearchStore = writable({
+    data: []
+});
+
+export const clearData = () => {
+    searchStore.set({
+        data: []
+    })
+}
+
+export const addData = (newData: Filedata[]) =>{
+    // clearData()
+    searchStore.set({
+        data: newData
+    })
+}
+
+export default searchStore;
