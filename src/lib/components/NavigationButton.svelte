84dc38d6a84ecc5e45aeb4c93f26060a73dd2de9
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-07-05 19:31:36 +0300

    Move navigation buttons to own file

---
+++ src/lib/components/NavigationButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let text: string;
+    export let disabled = false;
+    export let onClick: () => void;
+</script>
+
+<button disabled={disabled} on:click={onClick}>{text}</button>
+
+<style>
+    button {
+        background-color: #343434;
+        color: #eaeaea;
+        border: black solid 1px;
+        border-radius: 5px;
+        width: 2rem;
+        height: 2rem;
+        text-align: center;
+    }
+</style>
