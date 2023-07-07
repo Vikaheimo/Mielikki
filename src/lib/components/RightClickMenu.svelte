2c0780c9c2fb9f334c3e37d13ff86a2ef199401e
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-07-07 13:08:00 +0300

    Add css to RightClickMenu

--- src/lib/components/RightClickMenu.svelte
+++ src/lib/components/RightClickMenu.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import { text } from "@sveltejs/kit";
-    import type { MenuItem } from "./Types";
+    import type { MenuItem } from './Types';
 
     let pos = {
         x: 0,
@@ -21,38 +20,74 @@
 
     const openMenu = () => {
         displayMenu = true;
-    }
-
+    };
 </script>
 
 {#if displayMenu}
-<nav style="position: absolute; top:{pos.y}px; left:{pos.x}px">
-    <div>
+    <nav style="position: absolute; top:{pos.y}px; left:{pos.x}px">
         <ul>
             {#each menuItems as item}
-                {#if item.text === "hr"}
-                    <hr>
+                {#if item.text === 'hr'}
+                    <hr />
                 {:else}
                     <li>
                         <button on:click={item.onClick}>
-                            <i class={item.icon}></i>
+                            <i class={item.icon} />
                             {item.text}
                         </button>
                     </li>
                 {/if}
             {/each}
         </ul>
-    </div>
-</nav>
+    </nav>
 {/if}
 
-<svelte:window on:contextmenu|preventDefault={openMenu} on:click={closeMenu}/>
+<svelte:window on:contextmenu|preventDefault={openMenu} on:click={closeMenu} />
 
 <style>
-
-    hr{
+    hr {
         border: none;
-        border-bottom: 1px solid #ccc;
+        border-bottom: 1px solid black;
         margin: 5px 0px;
+    }
+
+    nav {
+        display: inline-flex;
+        border: 1px black solid;
+        width: 170px;
+        background-color: #2c2c2c;
+        border-radius: 10px;
+        overflow: hidden;
+        flex-direction: column;
+    }
+
+    ul {
+        margin: 6px;
+    }
+
+    li {
+        display: block;
+        list-style-type: none;
+        width: 1fr;
+    }
+
+    button {
+        color: #eaeaea;
+        font-size: 1rem;
+        width: 100%;
+        height: 30px;
+        text-align: left;
+        border: 0px;
+        background-color: #2c2c2c;
+    }
+
+    button:hover {
+        text-align: left;
+        border-radius: 5px;
+        background-color: #343434;
+    }
+
+    i {
+        padding: 0px 15px 0px 10px;
     }
 </style>
